d47c13856d8102f0bb09def1a2a496c5154f2ce9
yogeshyogi634 <yogeshyogi634@gmail.com>
2024-07-09 14:49:04 +0530

    contact us page updated

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -3,46 +3,61 @@
 import { motion } from "framer-motion";
 import { useRef, useState } from "react";
 import emailjs from "@emailjs/browser";
-import SwiperSlide from "@/components/appSwiper/appSwiper";
-import SwiperSlider from "@/components/appSwiper/appSwiper";
 
 function ContactPage() {
   const SERVICE_ID = process.env.NEXT_PUBLIC_SERVICE_ID;
   const TEMPLATE_ID = process.env.NEXT_PUBLIC_TEMPLATE_ID;
   const PUBLIC_KEY = process.env.NEXT_PUBLIC_PUBLIC_KEY;
 
-  // Example usage
   console.log(SERVICE_ID, TEMPLATE_ID, PUBLIC_KEY);
 
   const [success, setSuccess] = useState(false);
   const [error, setError] = useState(false);
+  const [errors, setErrors] = useState({});
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
   const text = "Say Hello!";
 
   const form = useRef();
+
+  const validateForm = () => {
+    const newErrors = {};
+    if (!email) {
+      newErrors.email = "Email is required";
+    } else if (!/\S+@\S+\.\S+/.test(email)) {
+      newErrors.email = "Email address is invalid";
+    }
+    if (!message) {
+      newErrors.message = "Message is required";
+    }
+    setErrors(newErrors);
+    return Object.keys(newErrors).length === 0;
+  };
 
   const sendEmail = (e) => {
     e.preventDefault();
     setError(false);
     setSuccess(false);
 
-    emailjs
-      .sendForm(
-        process.env.SERVICE_ID,
-        process.TEMPLATE_ID,
-        form.current,
-        process.PUBLIC_KEY
-      )
-      .then(
-        () => {
-          setSuccess(true);
-          form.current.reset();
-        },
-        () => {
-          console.error("EmailJS error: ", error);
-          setError(true);
-        }
-      );
+    if (!validateForm()) {
+      return;
+    }
+
+    emailjs.sendForm(SERVICE_ID, TEMPLATE_ID, form.current, PUBLIC_KEY).then(
+      (result) => {
+        console.log("Email sent successfully:", result.text);
+        setSuccess(true);
+        form.current.reset();
+        setEmail("");
+        setMessage("");
+      },
+      (error) => {
+        console.error("EmailJS error:", error.text);
+        setError(true);
+      }
+    );
   };
+
   return (
     <motion.div
       className="h-full"
@@ -50,9 +65,9 @@ function ContactPage() {
       animate={{ y: "0%" }}
       transition={{ duration: 1 }}
     >
-      <div className="h-full flex flex-col lg:flex-row px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48 ">
+      <div className="h-full flex flex-col lg:flex-row px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48">
         {/* TEXT CONTAINER */}
-        <div className="h-[30%]  lg:h-full lg:w-1/2 flex justify-center items-center text-4xl">
+        <div className="h-[30%] lg:h-full lg:w-1/2 flex justify-center items-center text-4xl">
           <div>
             {text.split("").map((letter, index) => (
               <motion.span
@@ -75,22 +90,51 @@ function ContactPage() {
         <form
           onSubmit={sendEmail}
           ref={form}
-          className="h-[60%] lg:h-[85vh] lg:w-1/2 bg-red-50 rounded-xl text-xl flex flex-col gap-8 justify-center p-10"
+          className="h-[70%] lg:h-[85vh] lg:w-1/2 bg-red-100 rounded-xl text-xl flex flex-col gap-8 justify-center p-10"
         >
-          {/* <span>Your Message:</span>
-          <textarea
-            rows={2}
-            className="bg-transparent border-b-2 border-b-black outline-none resize-none"
-            name="user_message"
-            required
-          /> */}
-          <span>Email:</span>
-          <input
-            name="user_email"
-            type="text"
-            className="bg-transparent border-b-2 border-b-black outline-none"
-            required
-          />
+          <div>
+            <label
+              htmlFor="message"
+              className="text-[#414141] text-14 font-normal"
+            >
+              Message:
+            </label>
+            <input
+              type="text"
+              name="message"
+              value={message}
+              onChange={(e) => {
+                setMessage(e.target.value);
+                setErrors({ ...errors, message: "" });
+              }}
+              className="w-full text-14 mt-6 focus:outline-none px-2 py-4"
+            />
+            {errors.message && (
+              <div className="text-red-500 text-lg mt-1">{errors.message}</div>
+            )}
+          </div>
+          <div>
+            <label
+              htmlFor="email"
+              className="text-[#414141] text-14 font-normal"
+            >
+              Email:
+            </label>
+            <input
+              type="email"
+              name="email"
+              value={email}
+              onChange={(e) => {
+                setEmail(e.target.value);
+                setErrors({ ...errors, email: "" });
+              }}
+              className="w-full text-14 mt-6 focus:outline-none px-2 py-4"
+            />
+
+            {errors.email && (
+              <div className="text-red-500 text-lg mt-1">{errors.email}</div>
+            )}
+          </div>
           <span>Regards</span>
           <button className="bg-purple-200 rounded font-semibold text-gray-600 p-4 hover:bg-purple-300">
             Send
